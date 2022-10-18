393f2f3d7d09db874b8d1d279246112aab85e299
EddyUMS <bi19110042@student.ums.edu.my>
2022-10-19 01:14:11 +0800

    Initial Commit

--- app/src/main/java/com/startagb/startagb/MainActivity2.java
+++ app/src/main/java/com/startagb/startagb/MainActivity2.java
@@ -17,7 +17,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.startlayout.farmer_login_pg);
+        setContentView(R.layout.farmer_login_pg);
         create_back_button();
     }
 
